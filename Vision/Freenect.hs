19210f0e7ab5498e8f6c1c75a445d0780f6dd3c5
Thomas M. DuBuisson <thomas.dubuisson@gmail.com>
2015-11-09 06:54:02 -0800

    Improved API.

--- Vision/Freenect.hs
+++ Vision/Freenect.hs
@@ -1,28 +1,56 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wall #-}
+
 module Vision.Freenect
   ( initialize
-  , withKinect
+  , withKinect, withKinect'
+  -- * Types
+  , Depth
+  , RunStatus(..), Config(..)
   ) where
 
+import Control.Concurrent (threadDelay)
 import Control.Monad.Fix
 import Vision.Freenect.Convert
 import Freenect
 import Data.IORef
 
+data RunStatus = Stop | Continue deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+-- | Structure for configuring the depth callback, method to acquire the
+-- run status (stopping the callbacks), and amount of delay between frame
+-- acquisition (in micro seconds).
+data Config = Config { depthCallback :: Depth -> IO ()
+                     , killSignal    :: IO RunStatus
+                     , delayTime     :: Int
+                     }
+
 -- | @withKinect (\kill img -> f kill img)@ will call @f@ continually
--- until @f@ calls @term@.  It is possible @f@ will be called after calling
--- @kill@, but the callbacks should stop \"soon\".
+-- until @f@ calls @kill@.  It is possible @f@ will be called after calling
+-- @kill@, but the callbacks should stop \"soon\".  There should be at most
+-- 10 callbacks per second (@delayTime=100000@).
 withKinect :: (IO () -> Depth -> IO ()) -> IO ()
-withKinect callback = do
-    continueRef <- newIORef True
-    let terminate = writeIORef continueRef False
+withKinect cb =
+  do continueRef <- newIORef Continue
+     let kill    = writeIORef continueRef Stop
+         cb'     = cb kill
+         killSig = readIORef continueRef
+     withKinect' Config { depthCallback = cb'
+                        , killSignal    = killSig
+                        , delayTime     = 100000 -- max of 10fps
+                        }
+
+withKinect' :: Config -> IO ()
+withKinect' (Config {..}) = do
     withContext $ \ctx -> do
        selectSubdevices ctx [Camera]
        withDevice ctx 0 $ \dev -> do
         setDepthMode dev Medium ElevenBit
-        let myCallback v _ = callback terminate (depthToFriday v)
+        let myCallback v _ = depthCallback (depthToFriday v)
         setDepthCallback dev myCallback
         startDepth dev
         fix $ \cont -> do
-                 c <- readIORef continueRef
-                 if c then processEvents ctx >> cont
-                      else return ()
+                 c <- killSignal
+                 if c == Continue
+                  then processEvents ctx >> threadDelay delayTime >> cont
+                  else return ()
